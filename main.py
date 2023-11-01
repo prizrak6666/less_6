08e790a0bfe18bac13bdb6348ae1eb93ead81454
syprayt <sypersyprayt@gmail.com>
2023-11-01 16:52:55 +0200

    less_6_1

---
+++ main.py
@@ -0,0 +1,20 @@
+try:
+    print("Початок коду")
+    print(10/0)
+    print("Немає помилок")
+except NameError:
+    print("В нас є помилка")
+except ZeroDivisionError:
+    print("Ми маємо ділення  на 0")
+
+print("Код після капсули")
+
+
+
+
+
+
+
+
+
+
